a7d8b6b4e7ae82f45470c56082f2d557c7978505
Dato <dato.egoyan@gmail.com>
2023-05-21 13:48:30 +0400

    Staged

--- FastFoodApplication/FastFood.Models/Order.cs
+++ FastFoodApplication/FastFood.Models/Order.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FastFood.Models
+﻿namespace FastFood.Models
 {
-    internal class Order
+    public class Order
     {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public Product Product { get; set; }
+        public Employee Employee { get; set; }
     }
 }
