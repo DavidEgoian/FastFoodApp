50fe896ad55224fa357ec09d764085d0e53bfe4a
Dato <dato.egoyan@gmail.com>
2023-05-26 22:11:40 +0400

    Headers Added

--- FastFoodApplication/FastFood.Models/Product.cs
+++ FastFoodApplication/FastFood.Models/Product.cs
@@ -1,10 +1,16 @@
-﻿namespace FastFood.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "ველის შევსება სავალდებულოა")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "ველის შევსება სავალდებულოა")]
         public double Price { get; set; }
+        [Required(ErrorMessage = "ველის შევსება სავალდებულოა")]
         public int Quantity { get; set; }
     }
 }
